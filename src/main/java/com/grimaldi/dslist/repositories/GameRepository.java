1ab73e37d8e244dbfd7df77d8b5b5af11ce816db
jggrimaldi <jgfg@cesar.school>
2025-08-18 20:07:03 -0300

    Game repository, service and controller

--- src/main/java/com/grimaldi/dslist/repositories/GameRepository.java
+++ src/main/java/com/grimaldi/dslist/repositories/GameRepository.java
@@ -1,8 +1,7 @@
 package com.grimaldi.dslist.repositories;
 
 import com.grimaldi.dslist.entities.Game;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Id> {
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
---
+++ src/main/java/com/grimaldi/dslist/services/GameService.java
@@ -0,0 +1,24 @@
+package com.grimaldi.dslist.services;
+
+import com.grimaldi.dslist.dto.GameMinDto;
+import com.grimaldi.dslist.entities.Game;
+import com.grimaldi.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDto> findAll(){
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDto> dto = result.stream()
+                .map(x -> new GameMinDto(x))
+                .toList();
+        return dto;
+    }
+}
